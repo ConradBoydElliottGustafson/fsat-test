98d630ab16e69296058b33daaa9475cece902d51
Greg Pascucci <greg.pascucci@gcgi.com>
2022-05-31 13:40:55 -0700

    added a quick unit test

--- backend/src/test/java/ca/cgi/fsa/FsaApplicationTests.java
+++ backend/src/test/java/ca/cgi/fsa/FsaApplicationTests.java
@@ -1,13 +1,21 @@
 package ca.cgi.fsa;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.cgi.fsa.entities.Person;
 
 @SpringBootTest
 class FsaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void createPerson() {
+		Person newPerson = new Person("Greg", "Pascucci", "greg.pascucci@gmail.com");
+		String firstName = "Greg";
+
+		// then
+		assertTrue(firstName.equals(newPerson.getFirstName()));
 	}
 
 }
