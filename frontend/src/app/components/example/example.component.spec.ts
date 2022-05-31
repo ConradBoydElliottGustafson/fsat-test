d3030d96d9cbe03c77a02ff7636821d611c55651
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2022-05-31 15:54:54 -0700

    - Address test case on example.component.

--- frontend/src/app/components/example/example.component.spec.ts
+++ frontend/src/app/components/example/example.component.spec.ts
@@ -1,25 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { FsaApiServiceService } from 'src/app/services/fsa-api-service.service';
 import { ExampleComponent } from './example.component';
+
 
 describe('ExampleComponent', () => {
   let component: ExampleComponent;
   let fixture: ComponentFixture<ExampleComponent>;
+  const stubCities = [{"cityName": "Powell River"}];
+  let getCitiesSpy;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ExampleComponent ]
+  beforeEach(() => {
+
+    // Service stub
+    const fsaApiService = jasmine.createSpyObj('FsaApiServiceService', ['getCities']);
+    getCitiesSpy = fsaApiService.getCities.and.returnValue(of(stubCities));
+
+    TestBed.configureTestingModule({
+      declarations: [ ExampleComponent ],
+      providers: [ { provide: FsaApiServiceService, useValue: fsaApiService } ]
     })
-    .compileComponents();
+
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExampleComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get cities after component initialized', () => {
+    fixture.detectChanges();  // onInit()
+  
+    // sync spy result shows testQuote immediately after init
+    expect(getCitiesSpy.calls.any())
+      .withContext('getCities called')
+      .toBe(true);
   });
 });
